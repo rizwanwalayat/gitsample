96aa84d2f0aaf17ac45a7b086a53599504af364c
Rizwan Walayat <rizwan.wallayat@gmail.com>
2020-05-09 16:58:36 +0500

    Initial commit

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Todos extends Component {
+    render(){
+  return (
+    <div >
+     <h1>Todos App</h1>
+    </div>
+  );
+    }
+}
+
+export default Todos;
